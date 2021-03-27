0f509e2e4ffc4c5e0e87096fe0c314d711a4c669
Priyanka <priyanka.23219@gmail.com>
2021-03-27 15:00:13 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {PortraitsComponent} from './portraits/portraits.component';
+import {WeddingComponent} from './wedding/wedding.component';
+ const routes: Routes = [
+  
+  {
+    path:"Home",
+    component: HomeComponent
+  },
+  {
+    path:"Portraits",
+    component: PortraitsComponent
+  },
+  {
+    path:"Wedding",
+    component: WeddingComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
